671b07423aa8a415b422783ad1039c7a533eb6be
Daria Petrova <daartpetrova@mail.ru>
2024-09-06 19:02:16 +0500

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import {MainLayout} from "./layout.tsx";
 
 import {AboutAuthor} from "@/pages/about-author/about-author.tsx";
@@ -10,14 +10,14 @@ import {Main} from "@/pages/main/main.tsx";
 import {Method} from "@/pages/method/method.tsx";
 
 
-const router = createBrowserRouter([
+const hashRouter = createHashRouter([
     {
         path: '/',
         element: <MainLayout/>,
         children: [
             {
-              path: '',
-              element: <Main/>
+                path: '',
+                element: <Main/>
             },
             {
                 path: 'calculator',
@@ -45,6 +45,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={hashRouter}/>
   </StrictMode>,
 )
